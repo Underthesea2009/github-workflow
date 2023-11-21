52993ccc7cc3449fe87bba66dd6ca6fc48fa096d
Underthesea2009 <131215067+Underthesea2009@users.noreply.github.com>
2023-11-21 18:50:49 -0500

    Create mockData.js

---
+++ mockData.js
@@ -0,0 +1,30 @@
+export const mockData = [
+  {
+    title: "Tom Sawyer",
+    author: "Mark Twain",
+    description: "An American classic.",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Tom_Sawyer_1876_frontispiece.jpg/440px-Tom_Sawyer_1876_frontispiece.jpg",
+    genre: "classic",
+    active: false,
+  },
+  {
+    title: "Tom Sawyer",
+    author: "Amy Tan",
+    description: "An asian perspective of life in America",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/en/c/ca/TheJoyLuckClub.jpg",
+    genre: "fiction",
+    active: false,
+  },
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    description:
+      "The unforgettable novel of a childhood in a sleepy Southern town and the crisis of conscience that rocked it. 'To Kill A Mockingbird' became both an instant bestseller and a critical success when it was first published in 1960. It went on to win the Pulitzer Prize in 1961 and was later made into an Academy Award-winning film, also a classic.",
+    imageUrl:
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1553383690i/2657.jpg",
+    genre: "novel",
+    active: false,
+  },
+];
